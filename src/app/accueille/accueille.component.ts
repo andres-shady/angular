96969c69ef1c76db8f32309faeb6674ee2e6697f
Mourchidiabdallah <abdallahmourchidi16@gmail.com>
2024-03-18 15:26:44 +0300

    ADD devoir angular

---
+++ src/app/accueille/accueille.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accueille',
+  standalone: true,
+  imports: [],
+  templateUrl: './accueille.component.html',
+  styleUrl: './accueille.component.css'
+})
+export class AccueilleComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProposComponent } from './propos/propos.component';
+import { AccueilleComponent } from './accueille/accueille.component';
+import { ProduitsComponent } from './produits/produits.component'; 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent,ProposComponent,AccueilleComponent,ProduitsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+
 })
 export class AppComponent {
   title = 'projet-angular';
